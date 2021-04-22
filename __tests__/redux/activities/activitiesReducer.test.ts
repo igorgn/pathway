5233d0ce22dd01b0c9cc1b7415cd7fa5e9a7b665
Vladislav Turdalijev <emapple1994@gmail.com>
2021-04-22 13:50:32 +0300

    added reducer test

---
+++ __tests__/redux/activities/activitiesReducer.test.ts
@@ -0,0 +1,41 @@
+/* eslint-env  jest */
+
+import {AnyAction} from 'redux';
+import activitiesReducer, {
+  addActivity,
+  initialActivitiesState,
+} from '../../../src/redux/activities/activitiesReducer';
+import {selectActivities} from '../../../src/redux/activities/activitiesSelectors';
+
+describe('activities slice', () => {
+  describe('reducer, actions and selectors', () => {
+    it('should return the initial state on first run', () => {
+      // Arrange
+      const nextState = initialActivitiesState;
+
+      // Act
+      const result = activitiesReducer(undefined, {} as AnyAction);
+
+      // Assert
+      expect(result).toEqual(nextState);
+    });
+
+    it('should properly set the state when sign in is made', () => {
+      // Arrange
+      const payload = {name: 'Activity three'};
+      const currentState = {
+        activities: [{name: 'Activity one'}, {name: 'Activity two'}],
+      };
+
+      // Act
+      const nextState = activitiesReducer(currentState, addActivity(payload));
+
+      // Assert
+      const rootState = {activities: nextState};
+      expect(selectActivities(rootState)).toEqual([
+        ...currentState.activities,
+        payload,
+      ]);
+    });
+  });
+});
