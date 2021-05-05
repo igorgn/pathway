c5bdbd05d8a6c6db0d32a4eca3efdf82dd4d7621
analogApple <emapple1994@gmail.com>
2021-05-05 09:29:05 +0300

    added main screen driver

--- src/screens/main/main-screen.driver.ts
+++ src/screens/main/main-screen.driver.ts
@@ -1,41 +1,14 @@
 import {Action, Store} from 'redux';
 import {ReduxComponentDriver} from 'redux-component-driver';
 import {RootState} from '../../redux/redux-store';
-import {
-  AddActivityScreenComponent,
-  ADD_ACTIVITY_SCREEN_TEST_IDS,
-} from './add-activity-screen';
+import {MainScreen} from './main-screen';
 
-export class AddActivityScreenDriver extends ReduxComponentDriver<
-  typeof AddActivityScreenComponent,
+export class MainScreenDriver extends ReduxComponentDriver<
+  typeof MainScreen,
   RootState,
   Action<any>
 > {
   constructor(store: Store) {
-    super(AddActivityScreenComponent, store);
-  }
-
-  getTextInput() {
-    return this.getByID(ADD_ACTIVITY_SCREEN_TEST_IDS.TEXT_FIELD);
-  }
-
-  getButton() {
-    return this.getByID(ADD_ACTIVITY_SCREEN_TEST_IDS.CTA);
-  }
-
-  getTextInputError() {
-    return this.getTextInput()?.props.error;
-  }
-
-  getTextInputValue() {
-    return this.getTextInput()?.props.value;
-  }
-
-  setInputText(text: string) {
-    this.getTextInput()?.props.onChangeText(text);
-  }
-
-  initiateActivityCreation() {
-    this.getButton()?.props.onPress();
+    super(MainScreen, store);
   }
 }
